e3fc7ef5f7e152d4118170ad8ab5414ce0247c1e
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-09-04 21:02:54 +0700

    add register route

--- BEMedsains/src/controllers/auth.controller.js
+++ BEMedsains/src/controllers/auth.controller.js
@@ -1,12 +1,18 @@
 const userRepository = require("../repositories/user.repositories");
 const { compare, hash } = require("../lib/bcrypt");
 
-//Register Controller
-const register = async (req, res) => {
+// Admin Register Controller
+const adminRegister = async (req, res) => {
   try {
-    const newUser = await userRepository.createUser(req.body);
+    // check user superadmin
+    const checkedUserRole = await userRepository.getUser({ role: 1 });
 
-    return res.status(200).send({ message: "register", data: newUser });
+    if (checkedUserRole) throw { message: "Unauthorized", statusCode: 401 };
+
+    const newUser = await userRepository.createUser({ ...req.body, role: 1 });
+    if (!newUser) throw { message: "Register Failed", statusCode: 500 };
+
+    return res.status(201).send({ message: "success register superadmin" });
   } catch (error) {
     return res.send({
       message: error.message || error,
@@ -16,7 +22,7 @@ const register = async (req, res) => {
   }
 };
 
-//Login Controller
+// Login Controller
 const login = async (req, res) => {
   try {
     // get User
@@ -29,13 +35,40 @@ const login = async (req, res) => {
     if (!checkPassword)
       throw { message: "username atau password salah", statusCode: 500 };
 
-    return res.status(200).send({ message: "Successfully logged in!" });
+    return res.status(201).send({ message: "Successfully logged in!" });
   } catch (error) {
     return res.status(500).send({ message: error.message || error });
   }
 };
 
+// Register Controller
+const register = async (req, res) => {
+  try {
+    if (+req.user.role !== 1) throw { message: "Unauthorize", statusCode: 401 };
+    // get user by email and username
+    const getUserByUsername = await userRepository.getUser(req.body.username);
+    if (getUserByUsername)
+      throw { message: "username is already exist", statusCode: 409 };
+
+    const getUserByEmail = await userRepository.getUser(req.body.email);
+    if (getUserByEmail)
+      throw { message: "email is already exist", statusCode: 409 };
+
+    const newUser = await userRepository.createUser(req.body);
+    if (!newUser) throw { message: "Register Failed", statusCode: 500 };
+
+    return res.status(201).send({ message: "Successfully Register" });
+  } catch (error) {
+    return res.send({
+      message: error.message || error,
+      status: "failed",
+      statusCode: error.statusCode,
+    });
+  }
+};
+
 module.exports = {
-  register,
+  adminRegister,
   login,
+  register,
 };
--- BEMedsains/src/routers/auth.router.js
+++ BEMedsains/src/routers/auth.router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { authController } = require("../controllers");
+const { auth } = require("../helpers/auth");
 const routers = express.Router();
 
-const { register, login } = authController;
+const { register, login, adminRegister } = authController;
 
-routers.post("/register", register);
+routers.post("/register", auth, register);
 routers.post("/login", login);
+routers.post("/admin-register", adminRegister);
 
 module.exports = routers;
