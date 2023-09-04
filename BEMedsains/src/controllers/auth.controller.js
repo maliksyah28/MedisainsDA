4b47be5c276e54fe39439b716e89f2ab751867b1
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-09-04 09:30:12 +0700

    dwi create login and register file structure

--- BEMedsains/src/controllers/auth.controller.js
+++ BEMedsains/src/controllers/auth.controller.js
@@ -1,19 +1,36 @@
+const userRepository = require("../repositories/user.repositories");
+const { compare, hash } = require("../lib/bcrypt");
+
 //Register Controller
 const register = async (req, res) => {
   try {
-    return res.status(200).send({ message: "register" });
+    const newUser = await userRepository.createUser(req.body);
+
+    return res.status(200).send({ message: "register", data: newUser });
   } catch (error) {
-    console.log(error);
-    return res.status(500).send({ message: error.message || error });
+    return res.send({
+      message: error.message || error,
+      status: "failed",
+      statusCode: error.statusCode,
+    });
   }
 };
 
 //Login Controller
 const login = async (req, res) => {
   try {
+    // get User
+    const getUser = await userRepository.getUser(req.body.userData);
+    if (!getUser)
+      throw { message: "username atau password salah", statusCode: 500 };
+
+    // Check Password
+    let checkPassword = compare(req.body.password, getUser.password);
+    if (!checkPassword)
+      throw { message: "username atau password salah", statusCode: 500 };
+
     return res.status(200).send({ message: "Successfully logged in!" });
   } catch (error) {
-    console.log(error);
     return res.status(500).send({ message: error.message || error });
   }
 };
---
+++ BEMedsains/src/repositories/user.repositories.js
@@ -0,0 +1,48 @@
+const { Account } = require("../../models");
+const { Op } = require("sequelize");
+const { compare, hash } = require("../lib/bcrypt");
+
+class UserRepository {
+  async createUser({ password, ...userData }) {
+    try {
+      const encryptedPassword = hash(password);
+      const resCreateUser = await Account.create({
+        ...userData,
+        password: encryptedPassword,
+      });
+      return await resCreateUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUserById(id) {
+    try {
+      return await Account.findOne({ where: { id } });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUser(userData) {
+    try {
+      return await Account.findOne({
+        where: { [Op.or]: [{ username: userData }, { email: userData }] },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      return await User.find();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Add other CRUD operations as needed
+}
+
+module.exports = new UserRepository();
