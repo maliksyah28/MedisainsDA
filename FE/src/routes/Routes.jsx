88f36be2c1de0f3ab5743242b8de69958e9e6545
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-09-13 13:27:59 +0700

    create signup admin

--- FE/src/routes/Routes.jsx
+++ FE/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import RegisterAdmin from "../pages/RegisterAdmin";
 
 function AppRoute() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/registerAdmin" element={<RegisterAdmin />} />
     </Routes>
   );
 }
